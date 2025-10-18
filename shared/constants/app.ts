251864c4bbc11091b9e3235b9e7fef5af9b19384
pedroarvl <pdroariasvilela@gmail.com>
2025-10-18 05:19:38 -0500

    fix(config): make Zod env validation environment-aware

--- shared/constants/app.ts
+++ shared/constants/app.ts
@@ -1,5 +1,6 @@
+import { env } from "@/config/env/env.scheme";
 
 export const APP = {
-  API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-  FRONTEND_BASE_URL: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
+  API_BASE_URL: env.client.NEXT_PUBLIC_API_BASE_URL,
+  FRONTEND_BASE_URL: env.client.NEXT_PUBLIC_FRONTEND_BASE_URL,
 };
